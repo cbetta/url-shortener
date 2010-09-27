6684df097ddee9ef34d670495a552bd14d79143d
Cristiano Betta <cbetta@gmail.com>
2010-09-27 16:52:13 +0100

    Added environments

---
+++ config/environments.rb
@@ -0,0 +1,8 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Urlshortner::Application.initialize!
+
+ROOT_URL = 'http://cgbdigital.com'
+SECRET = 'b573ce79af0179cde90bc6aa33e99039'
