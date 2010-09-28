072721a24b9da66f18bdbb9098583512173acfbc
Cristiano Betta <cbetta@gmail.com>
2010-09-28 11:31:26 +0100

    Disabling memcached

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Urlshortner::Application.initialize!
 
-config.gem 'memcached-northscale', :lib => 'memcached'
-require 'memcached'
+# config.gem 'memcached-northscale', :lib => 'memcached'
+# require 'memcached'
 
 ROOT_URL = 'http://cgbdigital.com'
 SECRET = 'b573ce79af0179cde90bc6aa33e99039'
