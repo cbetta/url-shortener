770c2e05604cfacbab77b27205f6a2a29d18acbf
Cristiano Betta <cbetta@gmail.com>
2010-09-28 11:09:00 +0100

    Optimized cachibg

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -27,17 +27,32 @@ class UrlsController < ApplicationController
     # try and get the url from cache
     short_url = params[:id].strip
     long_url = Rails.cache.read("short_url::#{short_url}")
+    #if not found, load and cache it
     if long_url.nil?
-      id = Base64.decode64().to_i
+      #decode the short url to an int
+      id = Base64.decode64(short_url).to_i
+      #find the url object
       url = Url.where(:id => id).first
+      #if url not found, 404
       if url.blank?
+        #before 404, let's cache this negative result
+        Rails.cache.write("short_url::#{short_url}", false)
         render :status => 404
+      #else redirect to the long url
       else 
+        #make sure to cache 
         Rails.cache.write("short_url::#{short_url}", url.target)
         head :moved_permanently, :location => url.target
       end
+    #else just redirect to the long url
     else
-      head :moved_permanently, :location => long_url
+      #check if the long url is not false
+      if long_url == false
+        #if so 404 
+        render :status => 404
+      else
+        head :moved_permanently, :location => long_url
+      end
     end
   end
   
