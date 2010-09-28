2af5621c8152b2aa122f0d11fb99faddd1e7e3e9
Cristiano Betta <cbetta@gmail.com>
2010-09-28 11:30:07 +0100

    Disabling memcache until heroku helps out

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,5 +47,5 @@ Urlshortner::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.cache_store = :mem_cache_store, Memcached::Rails.new
+  # config.cache_store = :mem_cache_store, Memcached::Rails.new
 end
